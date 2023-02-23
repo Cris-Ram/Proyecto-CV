193d9f21e1af00f610dc4378c67390c6376ac3e8
Cris-Ram <126106772+Cris-Ram@users.noreply.github.com>
2023-02-23 16:34:26 -0300

    Add files via upload
    Archivos de Mi CV

---
+++ Funcion.js
@@ -0,0 +1,12 @@
+
+document.getElementById("boton1").onclick = function () {
+    document.getElementById("uno").innerHTML = "7° Año";
+         
+}
+document.getElementById("boton2").onclick = function () {
+    document.getElementById("dos").innerHTML = "5° Año";
+}   
+document.getElementById("boton3").onclick = function () {
+    document.getElementById("tres").innerHTML = "Incompleto";
+    
+}   
